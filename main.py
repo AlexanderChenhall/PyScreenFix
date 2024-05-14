c258432b67b6856643373f9d6e5bd4877dc3ee58
alexanderchenhall <alexanderchenhall@protonmail.com>
2024-05-14 11:23:19 +1000

    Basic script to create a screen of random pixel values every tick.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 import sys
+import numpy as np
 
 if __name__ == "__main__":
     pygame.init()
@@ -9,7 +10,9 @@ if __name__ == "__main__":
     displayWidth = displayInfo.current_w
     displayHeight = displayInfo.current_h
 
-    screen = pygame.display.set_mode((displayWidth, displayHeight), pygame.FULLSCREEN)
+    # screen = pygame.display.set_mode((displayWidth, displayHeight), pygame.FULLSCREEN | pygame.HWACCEL)
+
+    screen = pygame.display.set_mode((displayWidth, displayHeight), pygame.HWACCEL)
 
     active = True
     clock = pygame.time.Clock()
@@ -19,3 +22,14 @@ if __name__ == "__main__":
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     active = False
+
+        pixel_array = np.random.randint(0, 256, (displayWidth, displayHeight, 3), dtype=np.uint8) #Generate random pixels
+        pixel_surface = pygame.surfarray.make_surface(pixel_array)
+        screen.blit(pixel_surface, (0, 0))
+        pygame.display.flip()
+        clock.tick(60)  # Limit FPS
+        pygame.time.delay(1)
+
+
+pygame.quit()
+sys.exit()
