acc8dac45f1b006a83528332101d3f4555e675d4
alexanderchenhall <alexanderchenhall@protonmail.com>
2024-05-14 12:56:04 +1000

    Added comments, so I can remember what everything does in 1 weeks time.

--- main.py
+++ main.py
@@ -20,19 +20,26 @@ if __name__ == "__main__":
                 if event.key == pygame.K_ESCAPE:
                     active = False
 
-        colors = [(255, 0, 0),      #red
-                  (0, 255, 0),      #blue
-                  (0, 0, 255),      #green
-                  (0, 0, 0,),       #black
-                  (255, 255, 255),  #white
-                  (0, 255, 255),    #cyan
-                  (255, 0, 255),    #magenta
-                  (255, 255, 0)]    #yellow
+        colors = [(255, 0, 0),      # Red       |     Declare tuples for RGB Values
+                  (0, 255, 0),      # Blue
+                  (0, 0, 255),      # Green
+                  (0, 0, 0,),       # Black
+                  (255, 255, 255),  # White
+                  (0, 255, 255),    # Cyan
+                  (255, 0, 255),    # Magenta
+                  (255, 255, 0)]    # Yellow
 
-        colors_2d = np.array(colors)
+        colors_2d = np.array(colors)          # |     Turn list of lists into a numpy array
+
+        # Creates an Array containing the indices for their corresponding tuples from colors
         pIndices = np.random.choice(len(colors_2d), size=(displayWidth, displayHeight))
+
+        # Create a new array by indexing colors_2d with pIndices
         pixel_array = colors_2d[pIndices]
+
+        # Create surface using new 3D array of randomly selected color tuples
         pixel_surface = pygame.surfarray.make_surface(pixel_array)
+
         screen.blit(pixel_surface, (0, 0))
         pygame.display.flip()
         clock.tick(60)  # Limit FPS
